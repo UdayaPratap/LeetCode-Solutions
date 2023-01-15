5012809ae1c3cf01f29f20a5f0a7edcb50630b47
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-01-15 16:34:12 +0530

    Create CountingBits.java

---
+++ CountingBits.java
@@ -0,0 +1,13 @@
+/*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+*/
+public class Solution {
+    public int[] countBits(int num) {
+        int[] result = new int[num + 1];
+        result[0] = 0;
+        for (int i = 1; i <= num; i++) {
+            result[i] = result[i&(i-1)] + 1;
+        }
+        return result;
+    }
+}
