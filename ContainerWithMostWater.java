dffef3743cb164eaca400d02294fe8775a9fb5eb
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-01-15 10:56:48 +0530

    Create ContainerWithMostWater.java

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,27 @@
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+*/
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0, right=height.length-1;
+        int maxArea=-1;
+        while(left<=right)
+        {
+            int area=(right-left)*Math.min(height[left],height[right]);
+            if(area>maxArea)
+                maxArea=area;
+            if(height[left]>height[right])
+                right--;
+            else
+                left++;
+            
+        }
+        return maxArea;
+    }
+}
