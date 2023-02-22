817da41d49fe6f98f6695df524dcc956cb48e7d3
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-02-22 11:39:59 +0530

    Update HouseRobber2.java

--- HouseRobber2.java
+++ HouseRobber2.java
@@ -3,7 +3,7 @@ You are a professional robber planning to rob houses along a street. Each house 
 
 Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 */
-
+//APPROACH 1 
 class Solution {
     public int rob(int[] nums) {
         return Math.max(rob(nums,false),rob(nums,true));
@@ -39,4 +39,32 @@ class Solution {
         return Math.max(dp[nums.length-1],dp[nums.length-2]);
     }
 
+}
+
+//APPROACH 2
+class Solution {
+    //call two times, picking first and not second, and vice versa.
+    //end changes depending on which we pick, if picking 0th: n-2 is end, else n-1 is end.
+    //we pick between start and end as all other are excluded.
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[0], nums[1]);
+        dp=new int[n+1][n+1];
+        for(int t[]: dp) Arrays.fill(t, -1);
+        int take0th=helper(0, n-2, nums);
+        int take1st=helper(1, n-1, nums);
+        return Math.max(take0th, take1st);
+    }
+    int dp[][];
+    int helper(int start, int end, int nums[])
+    {
+        if(start>end) return 0;
+        if(dp[start][end]!=-1) return dp[start][end];
+        //dont take current
+        int notTake=helper(start+1, end, nums);
+        //take
+        int take=nums[start]+helper(start+2, end, nums);
+        return dp[start][end]=Math.max(take, notTake);
+    }
 }
