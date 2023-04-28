882c712d093257c60ccb8163dc740a9fb9b86248
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-04-28 14:12:58 +0530

    Update LongestValidParantheses.java

--- LongestValidParantheses.java
+++ LongestValidParantheses.java
@@ -5,57 +5,24 @@ substring
 */
 class Solution {
     public int longestValidParentheses(String s) {
-        int maxLen=0;
-        int len=0;
-        int st=0;
-        int n=s.length();
-        //ITERATE FROM FIRST TO LAST
-                for(int i=0;i<n;i++)
-                {
-                    if(s.charAt(i)=='(')
-                    {
-                        st++;
-                        len++;
-                    }
-                    if(s.charAt(i)==')')
-                    {
-                        if(st==0)
-                        {
-                            len=0;
-                        }
-                        else{
-                            st--;
-                            len++;
-                        }
-                    }
-                    if(maxLen<len && st==0)
-                        maxLen=len;
-                }
-        len=0;
-        st=0;
-        //ITERATE FROM LAST TO FIRST
-        for(int i=n-1;i>=0;i--)
-        {
-            if(s.charAt(i)==')')
-            {
-                st++;
-                len++;
-            }
-            if(s.charAt(i)=='(')
-            {
-                if(st==0)
-                {
-                    len=0;
-                }
-                else{
-                    st--;
-                    len++;
-                }
-            }
-            if(maxLen<len && st==0)
-                maxLen=len;
-        }
-        
-        return maxLen;
+      Stack<Integer> st=new Stack<>();
+      st.push(-1); //stack shudnt be empty
+      int max=0;
+      for(int i=0;i<s.length();i++)
+      {
+          char c=s.charAt(i);
+          if(c=='(') st.push(i);
+          else if(c==')'){
+              st.pop();
+              if(st.isEmpty()){
+                  st.push(i);
+              }else{
+                int len=i-st.peek(); //length of valid string ending at i
+                max=Math.max(len, max);
+              }
+          }
+
+      }   
+      return max;
     }
 }
