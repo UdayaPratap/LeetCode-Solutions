6955ca62251f6cbe8310d47312500ed974644c0a
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-05-03 10:42:17 +0530

    Update Delete&Earn.java

--- Delete&Earn.java
+++ Delete&Earn.java
@@ -6,35 +6,28 @@ Return the maximum number of points you can earn by applying the above operation
 */
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        int n=nums.length;
         Arrays.sort(nums);
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++)
-            map.put(nums[i],i);
-        HashMap<Integer,Integer> freq=new HashMap<>();
-        for(int t: nums)
-            freq.put(t,freq.getOrDefault(t,0)+1);
-        int[] memo=new int[n+1];
-        return helper(nums,map,freq,memo,0);
+        int[] dp=new int[nums.length+1];
+        Arrays.fill(dp, -1);
+        return helper(nums, 0, dp);
     }
-    int helper(int[] nums, HashMap<Integer,Integer> map, HashMap<Integer,Integer> freq, int[] memo, int i)
+    int helper(int[] nums, int i, int[] dp)
     {
-        int n=nums.length;
-        if(i==n)
-            return 0;
-        if(memo[i]!=0)
-            return memo[i];
-        int del=0; int notdel=0;
-        
-        notdel=helper(nums,map,freq,memo,i+1);
-        if(map.containsKey(nums[i]+1))
-        {
-            int nextI=map.get(nums[i]+1);
-            int v=freq.get(nums[i]);
-            del=nums[i]*v+helper(nums,map,freq,memo,nextI+1);
-        }else{
-            del=nums[i]+helper(nums,map,freq,memo,i+1);
+        if(i==nums.length) return 0;
+        if(dp[i]!=-1) return dp[i];
+        //dont delete current
+        int a1=helper(nums, i+1, dp);
+        //delete current
+        //total sum of all occurences of current ith value
+        int sum=0, j=i;
+        while(j<nums.length && nums[j]==nums[i]){
+            sum+=nums[j];
+            j++;
         }
-        return memo[i]=Math.max(del,notdel);
+        //skip next as we cant include it
+        while(j<nums.length && nums[j]==nums[i]+1) j++;
+        int a2=sum+helper(nums, j, dp);
+        return dp[i]=Math.max(a1, a2);
+
     }
 }
