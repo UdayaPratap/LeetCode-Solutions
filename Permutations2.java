983adcd4a0e0227e401c55e4a71ff64bf9319697
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-05-12 14:09:10 +0530

    Update Permutations2.java

--- Permutations2.java
+++ Permutations2.java
@@ -4,32 +4,26 @@ Given a collection of numbers, nums, that might contain duplicates, return all p
  */
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
+        List<List<Integer>> ans= new ArrayList<>();
         Arrays.sort(nums);
-        helper(nums, new ArrayList<Integer>(), ans, new boolean[nums.length]);
+        helper(ans, new ArrayList<Integer>(), new boolean[nums.length], nums);
         return ans;
     }
-    void helper(int []nums, List<Integer> curr, List<List<Integer>> ans, boolean visited[])
+    void helper(List<List<Integer>> ans, List<Integer> curr, boolean[] visited, int[] nums)
     {
-        if(curr.size()==nums.length)
-        {
-            ans.add(new ArrayList<Integer>(curr));
+        if(curr.size()==nums.length){
+            ans.add(new ArrayList<>(curr));
             return;
         }
-
-        
-        for(int i=0;i<nums.length;i++)
+        for(int i=0; i<nums.length; i++)
         {
             if(visited[i]) continue;
-            if(i>0 && !visited[i-1] && nums[i]==nums[i-1]) continue;
-            
-                visited[i]=true;
-                curr.add(nums[i]);
-                helper(nums, curr, ans, visited);
-                curr.remove(curr.size()-1);
-                visited[i]=false;
-           
+            if(i>0 &&nums[i-1]==nums[i] && !visited[i-1]) continue;
+            visited[i]=true;
+            curr.add(nums[i]);
+            helper(ans, curr, visited, nums);
+            visited[i]=false;
+            curr.remove(curr.size()-1);
         }
-        return;
     }
 }
