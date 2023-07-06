dbee6ecde849f6e92eff5914a858fe857f4c332a
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-07-06 10:40:34 +0530

    Update SlidingWindowMax.java

--- SlidingWindowMax.java
+++ SlidingWindowMax.java
@@ -5,27 +5,18 @@ Return the max sliding window.
 */
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        List<Integer> ret=new ArrayList<>();
-        
-        PriorityQueue<int[]> pq=new PriorityQueue<>(Collections.reverseOrder(Comparator.comparing(a->a[0])));
-        for(int i=0;i<k;i++){
-            pq.add(new int[]{nums[i],i}); //store first 3 elemetns in heap
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{ return b[0]-a[0];});
+        //add first 3 elements to pq
+        for(int i=0; i<k; i++) pq.add(new int[]{nums[i], i});
+        int currMax=pq.peek()[0]; //max of these 3 elements
+        int[] ans=new int[nums.length-k+1];
+        int idx=0;
+        ans[idx++]=currMax;
+        for(int i=k; i<nums.length; i++){
+            while(pq.size()>0 && pq.peek()[1]<=(i-k)) pq.remove(); //remove out of bounds elements
+            pq.add(new int[]{nums[i], i}); //add ith element for ith window
+            ans[idx++]=pq.peek()[0]; //add max of ith window to ans
         }
-        
-        ret.add(pq.peek()[0]); //add max of first 3 to ret
-        int currMax=pq.peek()[0]; //currmax= max of first 3
-        
-        for(int i=k;i<nums.length;i++){
-            pq.add(new int[]{nums[i],i}); //add remaining elements one by one to heap and check max in below while loop
-            
-            while(pq.peek()[0]==currMax && pq.peek()[1]<=i-k){ //iterate till last slding window
-                pq.poll(); //remove the elelement that is out of bounds for current window
-                currMax=pq.peek()[0]; //currmax is the max of current window
-            }
-            currMax=pq.peek()[0]; 
-            ret.add(pq.peek()[0]);//add current max to ret
-        }
-        return ret.stream().mapToInt(x->x).toArray(); //ret to array
-
+        return ans;
     }
 }
