6f75abd8411addc7d24e17566d294b9950833881
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-08-07 11:47:15 +0530

    Create SetMatrixZeros.java

---
+++ SetMatrixZeros.java
@@ -0,0 +1,34 @@
+/*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.*/
+public class Solution {
+public void setZeroes(int[][] matrix) {
+    boolean fr = false,fc = false;
+    for(int i = 0; i < matrix.length; i++) {
+        for(int j = 0; j < matrix[0].length; j++) {
+            if(matrix[i][j] == 0) {
+                if(i == 0) fr = true; //to make first row 0
+                if(j == 0) fc = true; //to make first col 0
+                matrix[0][j] = 0; //to make jth row 0
+                matrix[i][0] = 0; //to make ith col 0
+            }
+        }
+    }
+    for(int i = 1; i < matrix.length; i++) {
+        for(int j = 1; j < matrix[0].length; j++) {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+        }}
+    }
+    if(fr) {
+        for(int j = 0; j < matrix[0].length; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+    if(fc) {
+        for(int i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+}}  
